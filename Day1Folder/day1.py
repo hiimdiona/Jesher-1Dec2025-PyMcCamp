099832df10bff107535ba983b00e0dfad6aade1c
hiimdiona <ng_jie_yu_jesher@students.edu.sg>
2025-12-01 08:51:47 +0000

    Dec 1, 2025, 8:51 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -4,38 +4,51 @@ print("hello from day1")
 
 ########################################################################
 # Task 1:
-
+print("helo again")
 
 
 ########################################################################
 # Task 2:
-
-
-
+print("My name is Jesher Ng")
+print("I am 13 years old")
+print("i attend ahmad ibrahim secondary school")
+print("If I had $2000 dollars, I would buy nintendo switch games and robux and save 1000 dollars")
 ########################################################################
 # Task 3:
-
+animal1 = "elephant"
+animal2 = "tiger"
+animal3 = "leopeard"
+print(animal1)
 
 
 ########################################################################
 # Task 4:
-
-
-
+variable1 = 3
+variable2 = 5
+print(variable1+variable2)
+print(variable1*variable2)
 ########################################################################
 # Task 5:
-
+name = "dog"
+food = "bone"
+print("my name is " + name)
+print("I like to eat " + food)
 
 
 ########################################################################
 # Task 6:
-
-
+def intro():
+    print("My name is Jesher Ng")
+    print("I am 13 years old")
+    print("i attend ahmad ibrahim secondary school")
+    print("If I had $2000 dollars, I would buy nintendo switch games and robux and save 1000 dollars")
+intro()
 
 ########################################################################
 # Task 7:
-
-
+def addition(number1, number2):
+    print(number1 + number2)
+addition(2, 3)
 
 ########################################################################
 # Additional exercises:
