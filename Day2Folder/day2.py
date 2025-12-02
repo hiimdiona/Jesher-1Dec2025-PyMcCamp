d93e58a25187239779de4a16ef20bc1f5fde6040
hiimdiona <ng_jie_yu_jesher@students.edu.sg>
2025-12-02 08:48:55 +0000

    Dec 2, 2025, 8:48 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -4,21 +4,35 @@ print("hello from day2")
 
 ########################################################################
 # Task 1:
-
-
-
+#for count in range(1000):
+    ##print ("world")
 ########################################################################
 # Task 2:
+#name="adam".upper()
+#for letter in name:
+   # print("give me a " + letter)
+#print("who is the best?")
+#print(name+("!"))
 
-
+#def cheer():
 
 ########################################################################
 # Task 3:
+## print(counter)
+
+for count in range(1,11,1):
+    print(count)
+for count in range(5,33,1):
+    print(count)
+
+for count in range(50,0,-1):
+    print(count)
 
 
 
 ########################################################################
 # Task 4:
+
 
 
 
@@ -34,3 +48,17 @@ print("hello from day2")
 
 ########################################################################
 # Additional exercises:
+for count in range(67):
+    print(count)
+for count in range(101):
+    print(count)
+for count in range(7,33):
+    print(count)
+for count in range(65,100):
+    print(count)
+for count in range(2,33,2):
+    print (count)
+for count in range(5,101,5):
+    print(count)
+for count in range(100,-1,-2):
+    print(count)
