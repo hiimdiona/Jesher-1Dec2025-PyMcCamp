af2416bf5c3ef19c5049e971cc34c68185a26534
hiimdiona <ng_jie_yu_jesher@students.edu.sg>
2025-12-03 07:25:30 +0000

    Dec 3, 2025, 7:25 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -37,7 +37,8 @@ age1=int(input("what is your age? "))
 age2+int(input("what is your age? "))
 if (age1 < age2):
     print("You are older than the previous person!")
-    
+else:
+    print
 
 
 ########################################################################
