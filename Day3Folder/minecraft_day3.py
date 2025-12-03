e6e9619926b5f4b164d6be9b773f83f934c224fe
hiimdiona <ng_jie_yu_jesher@students.edu.sg>
2025-12-03 08:52:05 +0000

    Dec 3, 2025, 8:52 AM

--- Day3Folder/minecraft_day3.py
+++ Day3Folder/minecraft_day3.py
@@ -1,2 +1,89 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def come():
+    agent.teleport_to_player()
+player.on_chat("come",come)
+def move_forward(steps):
+    agent.move(FORWARD,steps)
+
+player.on_chat("mf1",move_forward)
+
+def turn_left():
+    agent.turn(TurnDirection.LEFT)
+
+player.on_chat("tl1",turn_left)
+
+def move_backward(steps):
+    agent.move(BACKWARD,steps)
+    player.on_chat("mb1",move_backward)
+
+def turn_right():
+    agent.turn(TurnDirection.RIGHT)
+
+player.on_chat("tr1",turn_right)
+
+def move_up(steps):
+    agent.move(UP,steps)
+player.on_chat("mu1",move_up)
+
+def move_down(steps):
+    agent.move(DOWN,steps)
+player.on_chat("md1",move_down)
+
+
+def obby1():
+    agent.move(FORWARD,4)
+    agent.turn(LEFT)
+    agent.move(FORWARD,5)
+    agent.turn(RIGHT)
+    agent.move(FORWARD,3)
+    agent.move(UP,1)
+    agent.move(FORWARD,1)
+    agent.move(UP,1)
+    agent.move(FORWARD,1)
+    agent.move(UP,1)
+    agent.move(FORWARD,3)
+    agent.move(DOWN,1)
+    agent.move(FORWARD,1)
+    agent.move(DOWN,1)
+    agent.move(FORWARD,1)
+    agent.move(DOWN,1)
+    agent.move(FORWARD,1)
+player.on_chat("obby1",obby1)
+
+
+
+def choptree(height):
+    for count in range(8):
+        agent.destroy(FORWARD)
+        agent.move(UP,1)
+    agent.move(DOWN,6)
+    agent.collect_all()
+    agent.turn(RIGHT)
+    agent.move(FORWARD,1)
+    agent.turn(LEFT)
+    for count in range(8):
+        agent.destroy(FORWARD)
+        agent.move(UP,1)
+    agent.move(DOWN,6)
+    agent.collect_all()
+player.on_chat("choptree",choptree)
+
+def mine(length):
+    for count in range(26):
+        agent.destroy(LEFT)
+        agent.destroy(RIGHT)
+        agent.destroy(FORWARD)
+        agent.collect_all()
+        agent.move(FORWARD,1)
+player.on_chat("mine",mine)
+
+def build(length):
+    for count in range(length):
+        for count in range(31):
+            agent.place(DOWN)
+            agent.move(FORWARD, 1)
+        agent.move(UP,1)
+        agent.turn(LEFT)
+        agent.turn(LEFT)
+player.on_chat("build",build)
