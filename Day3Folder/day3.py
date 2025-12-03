41dfd1618fe63517c59f271abe5b349b270bb8dc
hiimdiona <ng_jie_yu_jesher@students.edu.sg>
2025-12-03 07:38:23 +0000

    Dec 3, 2025, 7:38 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -33,14 +33,14 @@ print("hello from day3")
 
 ########################################################################
 # Task 5:
-age1=int(input("what is your age? "))
-age2=int(input("what is your age? "))
-if age1==age2:
-    print("you all are the same age!")
-elif age1 < age2:
-    print("You are older than the previous person!")
-else:
-    print("you are younger than the previous person!")
+#age1=int(input("what is your age? "))
+#age2=int(input("what is your age? "))
+#if age1==age2:
+    #print("you all are the same age!")
+#elif age1 < age2:
+    #print("You are older than the previous person!")
+#else:
+    #print("you are younger than the previous person!")
 
 
 ########################################################################
